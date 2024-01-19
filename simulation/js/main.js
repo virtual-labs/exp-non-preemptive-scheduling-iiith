c149c4dd1b18aadaa2e3aa7acb438b8653084194
Yashpal-0 <yashpalyadav050@gmail.com>
2024-01-19 10:31:03 +0530

    controls on one side

--- simulation/js/main.js
+++ simulation/js/main.js
@@ -5,11 +5,8 @@ let State = {
     "Running":null,
     "Waiting":[],
     "Terminated":[],
-<<<<<<< HEAD
     "Map":{"id":null,"run_time":null,"burst_time":null},
     "Timer":null,
-=======
->>>>>>> 9ee49ac71e05a89afacbf21abc7db28cc489694c
     "Policy":"FCFS"
 };
 
@@ -17,21 +14,22 @@ let id_counter = 1;
 let time_counter = 0;
 
 class Process{
-    constructor(burst_time){
+    constructor(burst_time, status){
         this.burst_time = burst_time;
-<<<<<<< HEAD
+        this.status = status;
         this.mapping = {"run_time":0,"burst_time":burst_time}
-=======
-        this.map = [0,burst_time]
->>>>>>> 9ee49ac71e05a89afacbf21abc7db28cc489694c
         this.id = id_counter;
         id_counter++;
     }
 }
 
+function newProcess() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
 function CreateProcess(){
-    let create_process_input = document.getElementById("Burst_time_value").value;
-    document.getElementById("Burst_time_value").value = "";
+    let create_process_input = document.getElementById("burstTime").value;
+    document.getElementById("burstTime").value = "";
     if(create_process_input == ""){
         alert("Please enter a valid number");
         return;
@@ -45,30 +43,87 @@ function CreateProcess(){
         alert("Please enter the burst time less than or equal to 30")
         return
     }
-    let process = new Process(create_process_input_int);
+    let process = new Process(create_process_input_int, "Ready");
     Previous_States.push(State);
     State["Ready"].push(process);
     UpdateState();
     UpdateTable();
-<<<<<<< HEAD
-=======
-    console.clear();
-    console.log(Previous_States);
->>>>>>> 9ee49ac71e05a89afacbf21abc7db28cc489694c
+    newProcess();
 }
 
 function UpdateTable(){
-    let table = document.getElementById("ready_process")
-    table.innerHTML = "<th>Process ID</th><th>Burst Time</th>";
+    let table = document.getElementById("processes")
+    table.innerHTML = "<th>Process ID</th><th>Burst Time</th><th>Remaining time</th><th>Status</th>";
     // console.log(table);
+    if(State["Running"] != null) {
+        let row = table.insertRow(-1);
+        let cell1 = row.insertCell(0);
+        cell1.innerHTML = State["Running"].id;
+        let cell2 = row.insertCell(1);
+        cell2.innerHTML = State["Running"].mapping["burst_time"];
+        let cell3 = row.insertCell(2);
+        cell3.innerHTML = State["Running"].mapping["burst_time"] - State["Running"].mapping["run_time"];
+        let cell4 = row.insertCell(3);
+        cell4.innerHTML = State["Running"].status;
+        cell4.className = "tag-green";
+    }
     State["Ready"].forEach((process) => {
         let row = table.insertRow(-1);
         let cell1 = row.insertCell(0);
         cell1.innerHTML = process.id;
         let cell2 = row.insertCell(1);
         cell2.innerHTML = process.burst_time;
+        let cell3 = row.insertCell(2);
+        cell3.innerHTML = process.burst_time;
+        let cell4 = row.insertCell(3);
+        cell4.innerHTML = process.status;
+        cell4.className = "tag-orange";
     }
     );
+    State["Terminated"].forEach((process) => {
+        let row = table.insertRow(-1);
+        let cell1 = row.insertCell(0);
+        cell1.innerHTML = process.id;
+        let cell2 = row.insertCell(1);
+        cell2.innerHTML = process.burst_time;
+        let cell3 = row.insertCell(2);
+        cell3.innerHTML = 0;
+        let cell4 = row.insertCell(3);
+        cell4.innerHTML = process.status;
+        cell4.className = "tag-grey";
+    }
+    );
+}
+
+function schedule(){
+    if(State["Running"] == null) {
+        let cpuTable = document.getElementById("CPU")
+        let readyTable = document.getElementById("processes")
+        if(State["Ready"].length!=0){
+            cpuTable.innerHTML = "<th>Process ID</th><th>Burst Time</th><th>Run Time</th>";
+            // Add State["Ready"][0] to cpuTable
+            let row = cpuTable.insertRow(-1);
+            let cell1 = row.insertCell(0);
+            cell1.innerHTML = State["Ready"][0].id;
+            
+            let cell2 = row.insertCell(1);
+            cell2.innerHTML = State["Ready"][0].mapping["burst_time"];
+            let cell3 = row.insertCell(2);
+            cell3.innerHTML = State["Ready"][0].mapping["run_time"];
+        
+        
+        
+        
+            // Remove State["Ready"][0] from State["Ready"] and add to State["Running"]
+            tmp = State["Ready"][0];
+            State["Running"] = tmp;
+            
+            State["Ready"].shift();
+            State["Running"].status = "Running";
+            // UpdateState();
+        }
+    }
+    
 }
 
 function UpdateState(){
@@ -77,11 +132,8 @@ function UpdateState(){
     let TerminatedQueue = document.getElementById("tQ");
     let running = document.getElementById("cpu_p");
     let policy=document.getElementById("schd_p");
-<<<<<<< HEAD
     let map=document.getElementById("map");
-    let timer = document.getElementById("timer");
-=======
->>>>>>> 9ee49ac71e05a89afacbf21abc7db28cc489694c
+    let timer=document.getElementById("timer");
     readyQueue.innerHTML = "";
     temp = [];
     State["Ready"].forEach((process)=>{
@@ -103,7 +155,6 @@ function UpdateState(){
     running.innerHTML = "";
     if(State["Running"] == null){
         running.innerHTML = "None";
-<<<<<<< HEAD
         map.innerHTML = "";
     }
     else{
@@ -111,14 +162,14 @@ function UpdateState(){
         map.innerHTML = State["Running"].id + "->" + State["Running"].mapping["run_time"] + ":" + State["Running"].mapping["burst_time"];
         
     }
+    timer.innerHTML = State["Timer"];
     policy.innerHTML = State["Policy"];
-    timer
     
 }
 
-function Tick(){
-    if(State["Running"]==null){
-        SchedulePolicy();
+function Schedule(){
+    if(State["Policy"]=="FCFS"){
+        FCFS();
     }
     else{
         State["Running"].mapping["run_time"]++;
@@ -162,32 +213,57 @@ function SchedulePolicy(){
 }
 
 function FCFS(){
-    if(State["Ready"].length == 0){
-        return;
-    }
-    alert("Scheduling First Come First Serve(FCFS) Policy");
-    State["Running"] = State["Ready"].shift();
-    State["Timer"] = null;
-    UpdateState();
-    UpdateTable();
-}
-=======
-    }
-    else{
-        running.innerHTML = State["Running"].id;
-    }
-    policy.innerHTML = State["Policy"];
-}
-
-function Tick(){
-    if(State["Policy"]=="FCFS"){
-        FCFS();
-    }
-}
-
-function FCFS(){
     if(State["Running"]==null){
-        
+        if(State["Ready"].length==0){
+            return;
+        }
+        schedule();
+        UpdateState();
     }
 }
->>>>>>> 9ee49ac71e05a89afacbf21abc7db28cc489694c
+
+function ContextSwitch(){
+    if(State["Running"]!=null){
+        State["Waiting"].push(State["Running"]);
+        State["Running"] = null;
+        UpdateState();
+        UpdateTable();
+    }
+}
+
+function Terminate(){
+    if(State["Running"] != null) {
+        let cpuTable = document.getElementById("CPU")
+        tmp = State["Running"];
+        tmp.status = "Completed";
+        State["Terminated"].push(tmp)
+        State["Running"] = null;
+        cpuTable.deleteRow(0);
+        cpuTable.deleteRow(0);            
+    }
+}
+
+function Tick() {
+    
+    if(State["Running"]!=null){
+        time_counter++;
+        let ticker = document.getElementById("ticker");
+        ticker.innerHTML = time_counter;
+        State["Running"].mapping["run_time"]++;
+        State["Running"].mapping["burst_time"];
+        let cpuTable = document.getElementById("CPU")
+        // Update the second and third cells of the second row of cpuTable
+        cpuTable.rows[1].cells[1].innerHTML = State["Running"].mapping["burst_time"];
+        cpuTable.rows[1].cells[2].innerHTML = State["Running"].mapping["run_time"];
+        
+        if(State["Running"].mapping["burst_time"]==State["Running"].mapping["run_time"]){
+            Terminate();
+        }
+        UpdateTable();
+    }
+    
+    UpdateState();
+}
+
+
+
