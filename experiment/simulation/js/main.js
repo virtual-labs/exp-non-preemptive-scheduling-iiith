5502d15e8229504f9be0102d6becc05a61a79e0c
Yashpal-0 <yashpalyadav050@gmail.com>
2024-01-22 13:03:23 +0530

    resolved conflicts

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -2,25 +2,25 @@
 //Your JavaScript goes in here
 let Previous_States = [];
 let State = {
-    "Ready":[],
-    "Running":null,
-    "Waiting":[],
-    "Terminated":[],
+    "Ready": [],
+    "Running": null,
+    "Waiting": [],
+    "Terminated": [],
     "Completed": [],
-    "Map":{"id":null,"run_time":null,"burst_time":null},
-    "Timer":null,
-    "Policy":"FCFS",
-    "clickedState":null,
-    "time_counter":0
+    "Map": { "id": null, "run_time": null, "burst_time": null },
+    "Timer": null,
+    "Policy": "FCFS",
+    "clickedState": null,
+    "time_counter": 0
 };
 
 let id_counter = 1;
 
-class Process{
-    constructor(burst_time, status){
+class Process {
+    constructor(burst_time, status) {
         this.burst_time = burst_time;
         this.status = status;
-        this.mapping = {"run_time":0,"burst_time":burst_time}
+        this.mapping = { "run_time": 0, "burst_time": burst_time }
         this.id = id_counter;
         id_counter++;
     }
@@ -37,7 +37,7 @@ function assemble_msg(FEEDBACK, color) {
     var text = "";
 
     text += FEEDBACK;
-    
+
 
     td.innerHTML = text;
 
@@ -83,14 +83,14 @@ function openContent(toc_id) {
 }
 
 
-function loadUnloadCommand(cmd){
-    if(cmd=="schedule"){
-        if(State["clickedState"] == "schedule"){
+function loadUnloadCommand(cmd) {
+    if (cmd == "schedule") {
+        if (State["clickedState"] == "schedule") {
             State["clickedState"] = null;
             UpdateState();
         }
-        else if(State["clickedState"] == null){
-        State["clickedState"] = "schedule";
+        else if (State["clickedState"] == null) {
+            State["clickedState"] = "schedule";
             schd_btn = document.getElementById("schd-btn");
             schd_btn.classList.add("btn-loaded");
             // console.log(schd_btn.classList)
@@ -99,29 +99,29 @@ function loadUnloadCommand(cmd){
             sendalertalert("Please complete the previous command first or unselect it")
         }
     }
-    if(cmd=="newProcess"){
-        if(State["clickedState"] == "newProcess"){
+    if (cmd == "newProcess") {
+        if (State["clickedState"] == "newProcess") {
             State["clickedState"] = null;
             newProcess_btn = document.getElementById("newProcess-btn");
             newProcess_btn.classList.remove("btn-loaded");
         }
-        else if(State["clickedState"] == null){
-        State["clickedState"] = "newProcess";
-        UpdateState();
-        newProcess();
+        else if (State["clickedState"] == null) {
+            State["clickedState"] = "newProcess";
+            UpdateState();
+            newProcess();
         }
         else {
             sendalert("Please complete the previous command first or unselect it")
         }
     }
-    if(cmd=="terminate"){
-        if(State["clickedState"] == "terminate"){
+    if (cmd == "terminate") {
+        if (State["clickedState"] == "terminate") {
             State["clickedState"] = null;
             UpdateState();
         }
-        else if(State["clickedState"] == null){
-        State["clickedState"] = "terminate";
-        UpdateState();
+        else if (State["clickedState"] == null) {
+            State["clickedState"] = "terminate";
+            UpdateState();
         }
         else {
             sendalert("Please complete the previous command first or unselect it")
@@ -129,54 +129,53 @@ function loadUnloadCommand(cmd){
     }
 }
 
-function ToggleCreateProcess(){
-    if(State["clickedState"] == "newProcess"){
+function ToggleCreateProcess() {
+    if (State["clickedState"] == "newProcess") {
         State["clickedState"] = null;
         UpdateState();
         UpdateTable();
     }
-    else if(State["clickedState"] == null){
+    else if (State["clickedState"] == null) {
         let burst_input = document.getElementById("burstTime");
         document.getElementById("myDropdown").classList.toggle("show");
-        burst_input.placeholder = "Enter Burst Time (1-30) to create a process with Pid:"+id_counter;
-        
+        burst_input.placeholder = "Enter Burst Time (1-30) to create a process with Pid:" + id_counter;
+
     }
 }
 
 function newProcess() {
     document.getElementById("myDropdown").classList.toggle("show");
-    
+
 }
 
-function CreateProcess(){
+function CreateProcess() {
     let create_process_input = document.getElementById("burstTime").value;
     document.getElementById("burstTime").value = "";
-    if(create_process_input == ""){
+    if (create_process_input == "") {
         sendalert("Please enter a number between 1 to 30");
         return;
     }
     let create_process_input_int = parseInt(create_process_input);
-    if(create_process_input_int < 1){
+    if (create_process_input_int < 1) {
         sendalert("Please enter a number between 1 to 30");
         return;
     }
-    if(create_process_input_int > 30){
+    if (create_process_input_int > 30) {
         sendalertalert("Please enter a number between 1 to 30")
         return
     }
     let process = new Process(create_process_input_int, "Ready");
-    Previous_States.push(State);
     State["Ready"].push(process);
     UpdateState();
     UpdateTable();
     assemble_msg("New process created successfully!", "green");
 }
 
-function UpdateTable(){
+function UpdateTable() {
     let table = document.getElementById("processes")
     table.innerHTML = "<th>Process ID</th><th>Burst Time</th><th>Remaining time</th><th>Status</th>";
     // console.log(table);
-    if(State["Running"] != null) {
+    if (State["Running"] != null) {
         let row = table.insertRow(-1);
         let cell1 = row.insertCell(0);
         cell1.innerHTML = State["Running"].id;
@@ -229,30 +228,30 @@ function UpdateTable(){
     // );
 }
 
-function schedule(){
-    if(State["Running"] == null) {
+function schedule() {
+    if (State["Running"] == null) {
         let cpuTable = document.getElementById("CPU")
         let readyTable = document.getElementById("processes")
-        if(State["Ready"].length!=0){
+        if (State["Ready"].length != 0) {
             cpuTable.innerHTML = "<th>Process ID</th><th>Burst Time</th><th>Run Time</th>";
             // Add State["Ready"][0] to cpuTable
             let row = cpuTable.insertRow(-1);
             let cell1 = row.insertCell(0);
             cell1.innerHTML = State["Ready"][0].id;
-            
+
             let cell2 = row.insertCell(1);
             cell2.innerHTML = State["Ready"][0].mapping["burst_time"];
             let cell3 = row.insertCell(2);
             cell3.innerHTML = State["Ready"][0].mapping["run_time"];
-        
-        
-        
-        
+
+
+
+
             // Remove State["Ready"][0] from State["Ready"] and add to State["Running"]
             assemble_msg("Process with pid " + State["Ready"][0].id + " now running on the CPU!");
             tmp = State["Ready"][0];
             State["Running"] = tmp;
-            
+
             State["Ready"].shift();
             State["Running"].status = "Running";
             // UpdateState();
@@ -265,117 +264,118 @@ function schedule(){
     else {
         assemble_msg("A process is currently running on the CPU. Either wait for it to complete or terminate that process.", "red");
     }
-    
+
 }
 
-function UpdateState(){
-    let readyQueue= document.getElementById("rQ");
+function UpdateState() {
+    let readyQueue = document.getElementById("rQ");
     let waitingQueue = document.getElementById("wQ");
     let TerminatedQueue = document.getElementById("tQ");
     let running = document.getElementById("cpu_p");
-    let policy=document.getElementById("schd_p");
-    let map=document.getElementById("map");
-    let timer=document.getElementById("timer");
+    let policy = document.getElementById("schd_p");
+    let map = document.getElementById("map");
+    let timer = document.getElementById("timer");
     let ticker = document.getElementById("ticker");
     let schd_btn = document.getElementById("schd-btn");
     let newProcess_btn = document.getElementById("newProcess-btn");
     let end_btn = document.getElementById("end-btn");
+    let container = document.getElementById("prev_state");
     readyQueue.innerHTML = "";
     temp = [];
-    State["Ready"].forEach((process)=>{
+    State["Ready"].forEach((process) => {
         temp.push(process.id);
     })
-    readyQueue.innerHTML = "[ "+temp.join(",")+" ]";
+    readyQueue.innerHTML = "[ " + temp.join(",") + " ]";
     waitingQueue.innerHTML = "";
     temp = [];
-    State["Waiting"].forEach((process)=>{
+    State["Waiting"].forEach((process) => {
         temp.push(process.id);
     })
-    waitingQueue.innerHTML = "[ "+temp.join(",")+" ]";
+    waitingQueue.innerHTML = "[ " + temp.join(",") + " ]";
     TerminatedQueue.innerHTML = "";
     temp = [];
-    State["Terminated"].forEach((process)=>{
+    State["Terminated"].forEach((process) => {
         temp.push(process.id);
     })
-    TerminatedQueue.innerHTML = "[ "+temp.join(",")+" ]";
+    TerminatedQueue.innerHTML = "[ " + temp.join(",") + " ]";
     running.innerHTML = "";
-    if(State["Running"] == null){
+    if (State["Running"] == null) {
         running.innerHTML = "None";
         map.innerHTML = "";
     }
-    else{
+    else {
         running.innerHTML = State["Running"].id;
         map.innerHTML = State["Running"].id + "->" + State["Running"].mapping["run_time"] + ":" + State["Running"].mapping["burst_time"];
-        
+
     }
     timer.innerHTML = State["Timer"];
     policy.innerHTML = State["Policy"];
     ticker.innerHTML = State["time_counter"];
-    if(State["clickedState"] == "schedule"){
+    if (State["clickedState"] == "schedule") {
         schd_btn.classList.add("btn-loaded");
     }
-    else if(State["clickedState"] == "newProcess"){
+    else if (State["clickedState"] == "newProcess") {
         newProcess_btn.classList.add("btn-loaded");
     }
-    else if(State["clickedState"] == "terminate"){
+    else if (State["clickedState"] == "terminate") {
         end_btn.classList.add("btn-loaded");
     }
-    else if(State["clickedState"] == null){
+    else if (State["clickedState"] == null) {
         schd_btn.classList.remove("btn-loaded");
         newProcess_btn.classList.remove("btn-loaded");
         end_btn.classList.remove("btn-loaded");
     }
-    
+
 }
 
-function Schedule(){
-    if(State["Policy"]=="FCFS"){
+function Schedule() {
+    if (State["Policy"] == "FCFS") {
         FCFS();
     }
-    else{
+    else {
         State["Running"].mapping["run_time"]++;
         State["Timer"]--;
         UpdateState();
         UpdateTable();
-        if(State["Running"].mapping["burst_time"] == State["Running"].mapping["run_time"]){
+        if (State["Running"].mapping["burst_time"] == State["Running"].mapping["run_time"]) {
             State["Completed"].push(State["Running"]);
-            alert("Process "+State["Running"].id+" Completed");
+            alert("Process " + State["Running"].id + " Completed");
             State["Running"] = null;
             State["Timer"] = null;
             UpdateState();
             UpdateTable();
             SchedulePolicy();
         }
-        else if(State["Timer"] == 0){
+        else if (State["Timer"] == 0) {
             State["Ready"].push(State["Running"]);
             State["Running"] = null;
             State["Timer"] = null;
             alert("Timer Interrupt Occured");
             SchedulePolicy();
         }
-        
+
     }
-    
+
 }
 
-function SchedulePolicy(){
-    if(State["Policy"]=="FCFS"){
+function SchedulePolicy() {
+    if (State["Policy"] == "FCFS") {
         FCFS();
     }
-    else if(State["Policy"]=="SJF"){
+    else if (State["Policy"] == "SJF") {
         SJF();
     }
-    else if(State["Policy"]=="Priority"){
+    else if (State["Policy"] == "Priority") {
         Priority();
     }
-    else if(State["Policy"]=="Round Robin"){
+    else if (State["Policy"] == "Round Robin") {
         RoundRobin();
     }
 }
 
-function FCFS(){
-    if(State["Running"]==null){
-        if(State["Ready"].length==0){
+function FCFS() {
+    if (State["Running"] == null) {
+        if (State["Ready"].length == 0) {
             return;
         }
         schedule();
@@ -383,8 +383,8 @@ function FCFS(){
     }
 }
 
-function ContextSwitch(){
-    if(State["Running"]!=null){
+function ContextSwitch() {
+    if (State["Running"] != null) {
         State["Waiting"].push(State["Running"]);
         State["Running"] = null;
         UpdateState();
@@ -392,11 +392,11 @@ function ContextSwitch(){
     }
 }
 
-function Terminate( n = 1 ){
-    if(State["Running"] != null) {
+function Terminate(n = 1) {
+    if (State["Running"] != null) {
         let cpuTable = document.getElementById("CPU")
         tmp = State["Running"];
-        if(n==1){
+        if (n == 1) {
             tmp.status = "Terminated";
             State["Terminated"].push(tmp)
         }
@@ -407,7 +407,7 @@ function Terminate( n = 1 ){
         assemble_msg("Currently running process terminated succesfully", "green");
         State["Running"] = null;
         cpuTable.deleteRow(0);
-        cpuTable.deleteRow(0);            
+        cpuTable.deleteRow(0);
     }
     else {
         assemble_msg("No running process to terminate. Please schedule a process.", "red");
@@ -416,8 +416,8 @@ function Terminate( n = 1 ){
 }
 
 function Tick() {
-    if(State["clickedState"] == null){
-        if(State["Running"]!=null){
+    if (State["clickedState"] == null) {
+        if (State["Running"] != null) {
             State["time_counter"]++;
             let ticker = document.getElementById("ticker");
             ticker.innerHTML = State["time_counter"];
@@ -426,33 +426,68 @@ function Tick() {
             let cpuTable = document.getElementById("CPU")
             cpuTable.rows[1].cells[1].innerHTML = State["Running"].mapping["burst_time"];
             cpuTable.rows[1].cells[2].innerHTML = State["Running"].mapping["run_time"];
-            
-            if(State["Running"].mapping["burst_time"]==State["Running"].mapping["run_time"]){
+
+            if (State["Running"].mapping["burst_time"] == State["Running"].mapping["run_time"]) {
                 Terminate(0);
             }
             UpdateTable();
+            Previous_States.push(State);
         }
-    UpdateState();
+
+        UpdateState();
+        UpdatePreviousState();
     }
-    if(State["clickedState"] == "newProcess"){
+    if (State["clickedState"] == "newProcess") {
         CreateProcess();
         State["time_counter"]++;
         State["clickedState"] = null;
+        Previous_States.push(State);
         UpdateTable();
         UpdateState();
+        UpdatePreviousState();
     }
-    if(State["clickedState"] == "schedule"){
+    if (State["clickedState"] == "schedule") {
         schedule();
         State["time_counter"]++;
         State["clickedState"] = null;
+        Previous_States.push(State);
         UpdateTable();
         UpdateState();
+        UpdatePreviousState();
     }
-    if(State["clickedState"] == "terminate"){
+    if (State["clickedState"] == "terminate") {
         Terminate();
         State["time_counter"]++;
         State["clickedState"] = null;
+        Previous_States.push(State);
         UpdateTable();
         UpdateState();
+        UpdatePreviousState();
+    }
+
+}
+
+function UpdatePreviousState(){
+    let container=document.getElementById("prev_state");
+    container.innerHTML = "";
+    for(let i=Previous_States.length-1;i>=0;i--){
+        let state = Previous_States[i];
+        let button = document.createElement("button");
+        button.className = "collapsible";
+        button.innerHTML = "State " + (i+1);
+        button.onclick = createToggleFunction(i);
+        container.appendChild(button);
+        let table = document.createElement("table");
+        table.className = "content";
+        table.id = "state" + (i+1);
+        table.innerHTML = '<tr>    <td class="ts_cell">Ready:</td>    <td class="ts_cell" id="rQ"></td></tr><tr>    <td class="ts_cell">Waiting for I/O:</td>    <td class="ts_cell" id="wQ"></td></tr><tr>    <td class="ts_cell">Terminated:</td>    <td class="ts_cell" id="tQ"></td></tr><tr>    <td class="ts_cell">cpu:</td>    <td class="ts_cell" id="cpu_p"></td></tr><tr>    <td class="ts_cell">Map:</td>    <td class="ts_cell" id="map"></td></tr><tr>    <td class="ts_cell">Timer:</td>    <td class="ts_cell" id="timer"></td></tr><tr>    <td class="ts_cell">Scheduling policy:</td>    <td class="ts_cell" id="schd_p"></td></tr>'
+        container.appendChild(table);
+    }
+}
+
+function createToggleFunction(i){
+    return function(){
+        let content = document.getElementById("state" + (i+1));
+        content.classList.toggle("content");
     }
 }
